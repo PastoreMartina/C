59fac47ad24372c06bc6c474226bef3112018f1e
PastoreMartina <martina.pastore29@gmail.com>
2024-05-21 21:26:05 +0200

    Aggiunge ft_putnbr

---
+++ ft_putnbr.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb);
+
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+	ft_putnbr(2147483647);
+	write(1, "\n", 1);
+}
+
+void    ft_putnbr(int nb)
+{
+	int	nbr;
+	if (nb == -2147483648)
+	{
+		write(1, "-", 1);
+		write(1, "2", 1);
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		nbr = nb + '0';
+		write(1, &nbr, 1);
+	}
+}
